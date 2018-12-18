ee2b5b835dc147ee42dd9db8b5b423a84fd8a2d6
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-12-18 12:48:04 -0500

    changed laravel-backup schedule day & hour v1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,9 +25,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('backup:clean')
-                 ->weekly()->mondays()->at('18:00');
+                 ->weekly()->tuesdays()->at('13:00');
         $schedule->command('backup:run')
-                 ->weekly()->mondays()->at('18:00');
+                 ->weekly()->tuesdays()->at('13:30');
     }
 
     /**
